e83bead72c998cd583eef34df5a1b01ddb44ee69
mandalseva7318-cmd <mandalseva7318@gmail.com>
2026-05-12 11:41:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
 const upload = document.getElementById('upload');
-const preview = document.getElementById('preview');
-const compressBtn = document.getElementById('compressBtn');
+const previewImage = document.getElementById('previewImage');
+const imageCard = document.getElementById('imageCard');
+const oldSize = document.getElementById('oldSize');
+const newSize = document.getElementById('newSize');
 const downloadBtn = document.getElementById('downloadBtn');
+const compressBtn = document.getElementById('compressBtn');
+const deleteBtn = document.getElementById('deleteBtn');
+const qualityRange = document.getElementById('qualityRange');
+const qualityValue = document.getElementById('qualityValue');
 
 let selectedFile;
 
-upload.addEventListener('change', function (e) {
-  selectedFile = e.target.files[0];
-
-  if (selectedFile) {
-    preview.src = URL.createObjectURL(selectedFile);
-  }
+qualityRange.addEventListener('input', () => {
+  qualityValue.innerText = qualityRange.value + '%';
 });
 
-compressBtn.addEventListener('click', async function () {
+upload.addEventListener('change', (e) => {
 
-  if (!selectedFile) {
-    alert('Please upload image first');
+  selectedFile = e.target.files[0];
+
+  if(selectedFile){
+
+    imageCard.style.display = 'block';
+
+    previewImage.src = URL.createObjectURL(selectedFile);
+
+    oldSize.innerText = 'Original Size: ' + (selectedFile.size / 1024).toFixed(2) + ' KB';
+  }
+
+});
+
+compressBtn.addEventListener('click', async () => {
+
+  if(!selectedFile){
+    alert('Upload image first');
     return;
   }
 
+  const quality = qualityRange.value / 100;
+
   const options = {
-    maxSizeMB: 0.3,
-    maxWidthOrHeight: 1200,
-    useWebWorker: true,
+    maxSizeMB: quality,
+    maxWidthOrHeight: 1920,
+    useWebWorker: true
   };
 
   try {
+
     const compressedFile = await imageCompression(selectedFile, options);
+
+    newSize.innerText = 'Compressed Size: ' + (compressedFile.size / 1024).toFixed(2) + ' KB';
 
     const compressedURL = URL.createObjectURL(compressedFile);
 
     downloadBtn.href = compressedURL;
     downloadBtn.download = 'compressed-image.jpg';
-    downloadBtn.style.display = 'inline-block';
 
-    alert('Image Compressed Successfully');
-
-  } catch (error) {
-    console.log(error);
-  }
 });
